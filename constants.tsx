fe8f5138784788262a50645d46e3f06a8cb65abf
LucieMrc <lucie.mrc@gmail.com>
2025-12-23 12:22:48 +0100

    ajout videos

--- constants.tsx
+++ constants.tsx
@@ -6,45 +6,119 @@ export const ORNAMENTS: OrnamentData[] = [
   { 
     day: 1, id: 'o1', color: 'bg-red-500', position: { top: '15%', left: '50%' }, 
     videoUrls: [
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+      'calendrie_avent/medias/1_2023.mp4',
+      'calendrie_avent/medias/1_2025.mp4'
     ] 
   },
   { 
     day: 2, id: 'o2', color: 'bg-gold-500', position: { top: '25%', left: '42%' }, 
     videoUrls: [
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+      'calendrie_avent/medias/2_2024.mp4',
+      'calendrie_avent/medias/2_2025.mp4'
     ] 
   },
   { 
     day: 3, id: 'o3', color: 'bg-blue-500', position: { top: '25%', left: '58%' }, 
     videoUrls: [
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-      'https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
+      'calendrie_avent/medias/3_2025.mp4'
     ] 
   },
-  { day: 4, id: 'o4', color: 'bg-green-500', position: { top: '35%', left: '35%' }, videoUrls: ['https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4'] },
-  { day: 5, id: 'o5', color: 'bg-red-600', position: { top: '35%', left: '50%' }, videoUrls: ['https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackAds.mp4'] },
-  { day: 6, id: 'o6', color: 'bg-yellow-400', position: { top: '35%', left: '65%' }, videoUrls: ['https://storage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4'] },
-  { day: 7, id: 'o7', color: 'bg-emerald-500', position: { top: '45%', left: '28%' }, videoUrls: [] },
-  { day: 8, id: 'o8', color: 'bg-indigo-500', position: { top: '45%', left: '43%' }, videoUrls: [] },
-  { day: 9, id: 'o9', color: 'bg-rose-500', position: { top: '45%', left: '57%' }, videoUrls: [] },
-  { day: 10, id: 'o10', color: 'bg-amber-500', position: { top: '45%', left: '72%' }, videoUrls: [] },
-  { day: 11, id: 'o11', color: 'bg-cyan-500', position: { top: '55%', left: '22%' }, videoUrls: [] },
-  { day: 12, id: 'o12', color: 'bg-orange-500', position: { top: '55%', left: '36%' }, videoUrls: [] },
-  { day: 13, id: 'o13', color: 'bg-purple-500', position: { top: '55%', left: '50%' }, videoUrls: [] },
-  { day: 14, id: 'o14', color: 'bg-lime-500', position: { top: '55%', left: '64%' }, videoUrls: [] },
-  { day: 15, id: 'o15', color: 'bg-fuchsia-500', position: { top: '55%', left: '78%' }, videoUrls: [] },
-  { day: 16, id: 'o16', color: 'bg-red-400', position: { top: '65%', left: '15%' }, videoUrls: [] },
-  { day: 17, id: 'o17', color: 'bg-sky-500', position: { top: '65%', left: '32%' }, videoUrls: [] },
-  { day: 18, id: 'o18', color: 'bg-violet-500', position: { top: '65%', left: '50%' }, videoUrls: [] },
-  { day: 19, id: 'o19', color: 'bg-teal-500', position: { top: '65%', left: '68%' }, videoUrls: [] },
-  { day: 20, id: 'o20', color: 'bg-pink-500', position: { top: '65%', left: '85%' }, videoUrls: [] },
-  { day: 21, id: 'o21', color: 'bg-red-700', position: { top: '75%', left: '25%' }, videoUrls: [] },
-  { day: 22, id: 'o22', color: 'bg-amber-600', position: { top: '75%', left: '42%' }, videoUrls: [] },
-  { day: 23, id: 'o23', color: 'bg-emerald-600', position: { top: '75%', left: '58%' }, videoUrls: [] },
-  { day: 24, id: 'o24', color: 'bg-gold-600', position: { top: '75%', left: '75%' }, videoUrls: [] },
+
+  { day: 4, id: 'o4', color: 'bg-green-500', position: { top: '35%', left: '35%' }, videoUrls: [
+    'calendrie_avent/medias/4_2023.mp4',
+    'calendrie_avent/medias/4_2024.mp4',
+    'calendrie_avent/medias/4_2025.mp4'
+  ] },
+
+  { day: 5, id: 'o5', color: 'bg-red-600', position: { top: '35%', left: '50%' }, videoUrls: [
+    'calendrie_avent/medias/5_2023.mp4',
+    'calendrie_avent/medias/5_2024.mp4',
+    'calendrie_avent/medias/5_2025.mp4'
+  ] },
+
+  { day: 6, id: 'o6', color: 'bg-yellow-400', position: { top: '35%', left: '65%' }, videoUrls: [
+    'calendrie_avent/medias/6_2023.mp4',
+    'calendrie_avent/medias/6_2024.mp4'
+  ] },
+
+  { day: 7, id: 'o7', color: 'bg-emerald-500', position: { top: '45%', left: '28%' }, videoUrls: [
+    'calendrie_avent/medias/7_2023.mp4'
+  ] },
+
+  { day: 8, id: 'o8', color: 'bg-indigo-500', position: { top: '45%', left: '43%' }, videoUrls: [
+    'calendrie_avent/medias/8_2023.mp4',
+    'calendrie_avent/medias/8_2025.mp4'
+  ] },
+
+  { day: 9, id: 'o9', color: 'bg-rose-500', position: { top: '45%', left: '57%' }, videoUrls: [
+    'calendrie_avent/medias/9_2024.mp4',
+    'calendrie_avent/medias/9_2025.mp4'
+  ] },
+
+  { day: 10, id: 'o10', color: 'bg-amber-500', position: { top: '45%', left: '72%' }, videoUrls: [
+    'calendrie_avent/medias/10_2024.mp4',
+    'calendrie_avent/medias/10_2025.mp4'
+  ] },
+
+  { day: 11, id: 'o11', color: 'bg-cyan-500', position: { top: '55%', left: '22%' }, videoUrls: [
+    'calendrie_avent/medias/11_2023.mp4',
+    'calendrie_avent/medias/11_2025.mp4'
+  ] },
+
+  { day: 12, id: 'o12', color: 'bg-orange-500', position: { top: '55%', left: '36%' }, videoUrls: [
+    'calendrie_avent/medias/12_2023.mp4',
+    'calendrie_avent/medias/12_2024.mp4'
+  ] },
+
+  { day: 13, id: 'o13', color: 'bg-purple-500', position: { top: '55%', left: '50%' }, videoUrls: [
+    'calendrie_avent/medias/13_2023.mp4',
+    'calendrie_avent/medias/13_2024.mp4'
+  ] },
+
+  { day: 14, id: 'o14', color: 'bg-lime-500', position: { top: '55%', left: '64%' }, videoUrls: [
+    'calendrie_avent/medias/14_2023.mp4'
+  ] },
+
+  { day: 15, id: 'o15', color: 'bg-fuchsia-500', position: { top: '55%', left: '78%' }, videoUrls: [
+    'calendrie_avent/medias/15_2023.mp4'
+  ] },
+
+  { day: 16, id: 'o16', color: 'bg-red-400', position: { top: '65%', left: '15%' }, videoUrls: [
+    'calendrie_avent/medias/16_2025.mp4'
+  ] },
+
+  { day: 17, id: 'o17', color: 'bg-sky-500', position: { top: '65%', left: '32%' }, videoUrls: [
+    'calendrie_avent/medias/17_2024.mp4',
+    'calendrie_avent/medias/17_2025.mp4'
+  ] },
+
+  { day: 18, id: 'o18', color: 'bg-violet-500', position: { top: '65%', left: '50%' }, videoUrls: [
+    'calendrie_avent/medias/18_2023.mp4',
+    'calendrie_avent/medias/18_2024.mp4'
+  ] },
+
+  { day: 19, id: 'o19', color: 'bg-teal-500', position: { top: '65%', left: '68%' }, videoUrls: [
+    'calendrie_avent/medias/19_2023.mp4',
+    'calendrie_avent/medias/19_2024.mp4',
+    'calendrie_avent/medias/19_2025.mp4'
+  ] },
+
+  { day: 20, id: 'o20', color: 'bg-pink-500', position: { top: '65%', left: '85%' }, videoUrls: [
+    'calendrie_avent/medias/20_2023.mp4',
+    'calendrie_avent/medias/20_2024.mp4'
+
+  ] },
+
+  { day: 21, id: 'o21', color: 'bg-red-700', position: { top: '75%', left: '25%' }, videoUrls: [
+    'calendrie_avent/medias/21_2023.mp4'
+  ] },
+  { day: 22, id: 'o22', color: 'bg-amber-600', position: { top: '75%', left: '42%' }, videoUrls: [
+    'calendrie_avent/medias/22_2023.mp4'
+  ] },
+  { day: 23, id: 'o23', color: 'bg-emerald-600', position: { top: '75%', left: '58%' }, videoUrls: [
+
+  ] },
+  { day: 24, id: 'o24', color: 'bg-gold-600', position: { top: '75%', left: '75%' }, videoUrls: [
+
+  ] },
 ];
